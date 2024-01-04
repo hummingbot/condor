ae6627f1d6ef75e35cad8da29299b0eeed6cd560
cardosofede <federico.cardoso.e@gmail.com>
2024-01-04 17:28:22 -0300

    (feat) updated flow of create bot supporting selection of accounts

--- conversation_handlers/create_bot/create_bot.py
+++ conversation_handlers/create_bot/create_bot.py
@@ -9,7 +9,14 @@ from telegram.ext import (
 )
 
 # Define states
-ASKING_NAME, ASKING_IMAGE, CUSTOM_IMAGE, ASKING_SCRIPT, ASKING_CONFIG = range(5)
+(
+    ASKING_NAME,
+    ASKING_IMAGE,
+    CUSTOM_IMAGE,
+    ASKING_SCRIPT,
+    ASKING_CONFIG,
+    ASKING_CREDENTIALS,
+) = range(6)
 
 
 # Example list of available images and scripts
@@ -19,6 +26,8 @@ AVAILABLE_IMAGES = [
     "custom",
 ]
 AVAILABLE_SCRIPTS = ["script1", "script2", "script3"]
+AVAILABLE_CONFIGS = ["config1", "config2", "config3", "no"]
+AVAILABLE_CREDENTIALS = ["master_account", "test_account"]
 
 
 async def ask_bot_name(update: Update, context: CallbackContext) -> int:
@@ -31,7 +40,9 @@ async def ask_image(update: Update, context: CallbackContext) -> int:
         [InlineKeyboardButton(image, callback_data=image)] for image in AVAILABLE_IMAGES
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("Choose a Docker image:", reply_markup=reply_markup)
+    await update.message.reply_text(
+        "🐙 Choose a docker image for your bot:", reply_markup=reply_markup
+    )
     return ASKING_IMAGE
 
 
@@ -40,42 +51,53 @@ async def image_chosen(update: Update, context: CallbackContext) -> int:
     await query.answer()
 
     if query.data == "custom":
-        await query.edit_message_text(text="Please enter the custom Docker image name:")
+        await query.edit_message_text(text="Please enter the custom docker image name:")
         return CUSTOM_IMAGE
     else:
         context.user_data["image"] = query.data
         await query.edit_message_text(text=f"Selected image: {query.data}")
-        await update.callback_query.message.reply_text(
-            "Do you want to autostart the bot with a script? (yes/no)"
-        )
-        return ASKING_SCRIPT
+        await present_available_credentials(update, context)
+        return ASKING_CREDENTIALS
 
 
 async def custom_image(update: Update, context: CallbackContext) -> int:
     context.user_data["image"] = update.message.text
-    await update.message.reply_text(
-        f"Custom image set to: {update.message.text}\nDo you want to autostart the bot with a script? (yes/no)"
+    await update.message.reply_text(f"Custom image set to: {update.message.text}")
+    await present_available_credentials(update, context)
+    return ASKING_CREDENTIALS
+
+
+async def present_available_scripts(update: Update, context: CallbackContext) -> int:
+    keyboard = [
+        [InlineKeyboardButton(script, callback_data=script)]
+        for script in AVAILABLE_SCRIPTS
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.callback_query.message.reply_text(
+        "Choose a script to run:", reply_markup=reply_markup
     )
+
+
+async def present_available_credentials(
+    update: Update, context: CallbackContext
+) -> int:
+    keyboard = [
+        [InlineKeyboardButton(credential, callback_data=credential)]
+        for credential in AVAILABLE_CREDENTIALS
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.callback_query.message.reply_text(
+        "Choose the credentials to use:", reply_markup=reply_markup
+    )
+
+
+async def ask_credentials(update: Update, context: CallbackContext) -> int:
+    query = update.callback_query
+    await query.answer()
+    context.user_data["credentials"] = query.data
+    await query.edit_message_text(text=f"Selected Credentials: {query.data}")
+    await present_available_scripts(update, context)
     return ASKING_SCRIPT
-
-
-async def ask_script(update: Update, context: CallbackContext) -> int:
-    script_response = update.message.text.lower()
-    if script_response == "yes":
-        keyboard = [
-            [InlineKeyboardButton(script, callback_data=script)]
-            for script in AVAILABLE_SCRIPTS
-        ]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        await update.message.reply_text(
-            "Choose a script to autostart:", reply_markup=reply_markup
-        )
-    else:
-        context.user_data["script"] = None
-        await update.message.reply_text(
-            "Do you want to autostart the script with a config? (yes/no)"
-        )
-        return ASKING_CONFIG
 
 
 async def script_chosen(update: Update, context: CallbackContext) -> int:
@@ -83,23 +105,31 @@ async def script_chosen(update: Update, context: CallbackContext) -> int:
     await query.answer()
     context.user_data["script"] = query.data
     await query.edit_message_text(text=f"Selected script: {query.data}")
+    keyboard = [
+        [InlineKeyboardButton(config, callback_data=config)]
+        for config in AVAILABLE_CONFIGS
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
     await update.callback_query.message.reply_text(
-        "Do you want to autostart the script with a config? (yes/no)"
+        "Do you want to autostart the script with a config?", reply_markup=reply_markup
     )
     return ASKING_CONFIG
 
 
 async def ask_config(update: Update, context: CallbackContext) -> int:
-    config_response = update.message.text.lower()
-    context.user_data["config"] = config_response == "yes"
+    query = update.callback_query
+    await query.answer()
+    config_response = None if query.data == "no" else query.data
+    await query.edit_message_text(text=f"Selected config: {query.data}")
+    context.user_data["config"] = config_response
     return await summarize_and_end(update, context)
 
 
 async def summarize_and_end(update: Update, context: CallbackContext) -> int:
     bot_name = context.user_data.get("bot_name", "N/A")
     image = context.user_data.get("image", "N/A")
-    script = context.user_data.get("script", "No")
-    config = "Yes" if context.user_data.get("config", False) else "No"
+    script = context.user_data.get("script", "N/A")
+    config = context.user_data.get("config", "N/A")
 
     reply_text = (
         "Bot Creation Summary:\n"
@@ -109,7 +139,7 @@ async def summarize_and_end(update: Update, context: CallbackContext) -> int:
         f"- Autostart script with config: {config}\n"
         "\nCreating bot..."
     )
-    await update.message.reply_text(reply_text)
+    await update.callback_query.message.reply_text(reply_text)
     # Here you would add the logic to create the bot based on the collected data
 
     return ConversationHandler.END
@@ -122,20 +152,22 @@ async def cancel(update: Update, context: CallbackContext) -> int:
 
 def get_create_bot_conversation_handler() -> ConversationHandler:
     return ConversationHandler(
-        entry_points=[CommandHandler("create_bot", ask_bot_name)],
+        entry_points=[CommandHandler("create_bot", ask_image)],
         states={
-            ASKING_NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, ask_image)],
             ASKING_IMAGE: [CallbackQueryHandler(image_chosen)],
             CUSTOM_IMAGE: [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, custom_image)
+                MessageHandler(
+                    filters.TEXT & ~filters.COMMAND & ~filters.Regex("(?i)^cancel$"),
+                    custom_image,
+                )
+            ],
+            ASKING_CREDENTIALS: [
+                CallbackQueryHandler(ask_credentials),
             ],
             ASKING_SCRIPT: [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, ask_script),
                 CallbackQueryHandler(script_chosen),
             ],
-            ASKING_CONFIG: [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, ask_config)
-            ],
+            ASKING_CONFIG: [CallbackQueryHandler(ask_config)],
         },
         fallbacks=[CommandHandler("cancel", cancel)],
     )
